33631ba9dbc00f299c34516c6f406c1bf5beb2c0
Tristan Anderson <56132462+TAnderson2023@users.noreply.github.com>
2022-01-24 06:14:02 -0600

    finished L System Fractal visualisation

---
+++ javascript/p5js/LSystemFractalTree/sketch.js
@@ -0,0 +1,72 @@
+var angle;
+var axiom = 'F';
+var sentence = axiom;
+var len = 100;
+
+var rules = [];
+rules[0] = {
+  a: 'F',
+  b: 'FF+[+F-F-F]-[-F+F+F]'
+};
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+function generate() {
+  len *= 0.5;
+  var nextSentence = '';
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+    var found = false;
+    for (var j = 0; j < rules.length; j++) {
+      if (current == rules[j].a) {
+        found = true;
+        nextSentence += rules[j].b;
+        break;
+      }
+    }
+    if (!found) {
+      nextSentence += current;
+    }
+  }
+  sentence = nextSentence;
+  turtle();
+}
+
+function turtle() {
+  background(51);
+  resetMatrix();
+  translate(width / 2, height);
+  stroke(255, 100);
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+
+    if (current == 'F') {
+        line(0, 0, 0, -len);
+        translate(0, -len);
+    } else if (current == '+') {
+      rotate(angle);
+    } else if (current == '-') {
+      rotate(-angle);
+    } else if (current == '[') {
+      push();
+    } else if (current == ']') {
+      pop();
+    }
+  }
+}
+
+function setup() {
+  createCanvas(400, 400);
+  angle = radians(25);
+  background(51);
+  turtle();
+  for(let i = 0; i < 5; i++){
+    generate();
+  }
+}
